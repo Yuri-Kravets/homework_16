d8c78a599ebf59b7f5868eecf3b94f3299439eed
Yuri-Kravets <kravets.yuri.v@gmail.com>
2021-10-22 15:32:40 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-
-function getSqrt(x,y) {
-  let z = Math.sqrt((x ** 2) + (y ** 2));
-  console.log(z);
-  (z < r) ? alert('точка в окружности') : alert('точка вне окружности');
-}
-
+let x = 4;
+let y = 9;
+let z;
 const r = 10;
-getSqrt(3,2);
 
+z = Math.sqrt((x ** 2) + (y ** 2));
+(z < r) ? alert('точка в окружности') : alert('точка вне окружности');
 
